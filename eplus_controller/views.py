0844be45d63e06a0ca2b2c78cf0a76ad94896018
Edwin Lee <leeed2001@gmail.com>
2021-04-29 07:40:04 -0500

    Update for cross platform paths and such

--- eplus_controller/views.py
+++ eplus_controller/views.py
@@ -1,5 +1,6 @@
 from json import loads
 from os import makedirs, path
+from platform import system
 from shutil import rmtree
 import sys
 from time import sleep
@@ -9,18 +10,59 @@ from django.http import JsonResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 
-# some constants
-ProductsDir = '/eplus/installs/EnergyPlus-9-5-0'
-RepoRoot = '/eplus/installs/EnergyPlus-9-5-0'
-IDFDir = 'ExampleFiles'
-# TestFileName = '5ZoneAirCooled.idf'
-# ZoneName = 'Space1-1'
-TestFileName = '1ZoneUncontrolled.idf'
-ZoneName = 'Zone One'
+
+class RunConfig:
+    Install = 1
+    Build = 2
+    Current = Install
+
+
+class FileToRun:
+    OneZoneUncontrolled = 1
+    FiveZoneAirCooled = 2
+    Current = OneZoneUncontrolled
+
+
+if RunConfig.Current == RunConfig.Install:
+    IDFDir = 'ExampleFiles'
+    EPWDir = 'WeatherData'
+else:  # Assume Build
+    IDFDir = 'testfiles'
+    EPWDir = 'weather'
+
+if system() == 'Linux':
+    if RunConfig.Current == RunConfig.Install:
+        ProductsDir = '/eplus/installs/EnergyPlus-9-5-0'
+        RepoRoot = '/eplus/installs/EnergyPlus-9-5-0'
+    else:  # Assume Build
+        ProductsDir = '/eplus/repos/1eplus/builds/r/Products'
+        RepoRoot = '/eplus/repos/1eplus'
+elif system() == 'Darwin':
+    if RunConfig.Current == RunConfig.Install:
+        ProductsDir = '/Applications/EnergyPlus-9-5-0'
+        RepoRoot = '/Applications/EnergyPlus-9-5-0'
+    else:  # Assume Build
+        ProductsDir = '/Users/elee/eplus/repos/1eplus/builds/r/Products'
+        RepoRoot = '/Users/elee/eplus/repos/1eplus'
+else:  # Assume Windows
+    if RunConfig.Current == RunConfig.Install:
+        ProductsDir = 'C:/EnergyPlus-9-5-0'
+        RepoRoot = 'C:/EnergyPlus-9-5-0'
+    else:  # Assume Build
+        ProductsDir = 'C:/EnergyPlus/repos/1eplus/builds/VS64/Products/Release'
+        RepoRoot = 'C:/EnergyPlus/repos/1eplus'
+
+if FileToRun.Current == FileToRun.OneZoneUncontrolled:
+    TestFileName = '1ZoneUncontrolled.idf'
+    ZoneName = 'Zone One'
+else:  # Assume 5ZoneAirCooled
+    TestFileName = '5ZoneAirCooled.idf'
+    ZoneName = 'Space1-1'
+
 
 # Here are the living variables that we will use in the server
 eplus_outdoor_temp = 23.3
-eplus_output = b"HERE IA M"
+eplus_output = b"(No Output Yet)"
 eplus_progress = 0
 got_handles = False
 oa_temp_actuator = -1
@@ -108,7 +150,7 @@ def thread_function():
             working_dir,
             '-a',
             '-w',
-            '/eplus/epw/chicago.epw',
+            path.join(RepoRoot, EPWDir, 'ChicagoNoSolar.epw'),  # 'USA_IL_Chicago-OHare.Intl.AP.725300_TMY3.epw'),
             path.join(RepoRoot, IDFDir, TestFileName)
         ]
     )
